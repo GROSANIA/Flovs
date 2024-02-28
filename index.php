37fdee4ab264e0c51ae36d7b32608db488771690
GROSANIA <88773053+GROSANIA@users.noreply.github.com>
2024-02-28 10:28:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/index.css">
+    <title>Flovs</title>
+</head>
+<body>
+    <main>
+        <div class="Banner">
+            <h1></h1>
+        </div>
+        <div class="Container">
+            <h2>Flovs</h2>
+            <div class="InnerContainer">
+                <a href="login.php" class="Login">Login</a>
+                <a href="register.php" class="Daftar">Register</a>
+            </div>
+        </div>
+    </main>
+    <script src="asset/js/script.js"></script>
+</body>
+</html>
